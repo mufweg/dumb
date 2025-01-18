fbaa169d7cb52371106884c56b8182facbb4c510
alexandra <alexandra.kaiser.2001@gmail.com>
2025-01-18 17:52:02 +0100

    test commit

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,6 +3,6 @@ package org.example;
 public class Test {
     //new commit
     public static void main(String[] args) {
-        System.out.print("Test git");
+        System.out.print("Test git new");
     }
 }
